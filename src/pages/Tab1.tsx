804e9f66aa7ab7d8f073f4f52cfc5dabba3a8ed8
Daniel <daniel@daniels-air.speedport.ip>
2024-08-20 21:03:15 +0200

    init

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import LiveStream from '../components/Livestream';
 
 const Tab1: React.FC = () => {
   return (
@@ -16,7 +17,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <LiveStream />
       </IonContent>
     </IonPage>
   );
