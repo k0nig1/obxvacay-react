198bd9e7789b778e298d34b102bb1c8a7ca78795
Daniel Holterhaus <daniel.holterhaus@gmail.com>
2024-10-04 11:34:40 +0200

    changed social media icons to linkedin and pintrest

--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { IonContent, IonButton, IonIcon } from '@ionic/react';
-import { logoFacebook, logoInstagram, logoTwitter, logoYoutube } from 'ionicons/icons';
+import { IonButton, IonIcon } from '@ionic/react';
+import { logoFacebook, logoTwitter, logoLinkedin, logoPinterest } from 'ionicons/icons';
 import './SocialMediaIcons.css'; // Include this CSS file for styling
 
 const SocialMediaIcons: React.FC = () => {
   return (
-      <div className="social-media-icons">
-        <IonButton href="https://www.facebook.com/obxvacay" target="_blank" fill="clear">
-          <IonIcon icon={logoFacebook} slot="icon-only" />
-        </IonButton>
-        <IonButton href="https://www.instagram.com/obxvacay" target="_blank" fill="clear">
-          <IonIcon icon={logoInstagram} slot="icon-only" />
-        </IonButton>
-        <IonButton href="https://twitter.com/obxvacay" target="_blank" fill="clear">
-          <IonIcon icon={logoTwitter} slot="icon-only" />
-        </IonButton>
-        <IonButton href="https://www.youtube.com/obxvacay" target="_blank" fill="clear">
-          <IonIcon icon={logoYoutube} slot="icon-only" />
-        </IonButton>
-      </div>
+    <div className="social-media-icons">
+      <IonButton href="https://www.facebook.com/obxvacay" target="_blank" fill="clear">
+        <IonIcon icon={logoFacebook} slot="icon-only" />
+      </IonButton>
+      <IonButton href="https://twitter.com/obxvacay" target="_blank" fill="clear">
+        <IonIcon icon={logoTwitter} slot="icon-only" />
+      </IonButton>
+      <IonButton href="https://www.linkedin.com/company/obxvacay" target="_blank" fill="clear">
+        <IonIcon icon={logoLinkedin} slot="icon-only" />
+      </IonButton>
+      <IonButton href="https://www.pinterest.com/obxvacay" target="_blank" fill="clear">
+        <IonIcon icon={logoPinterest} slot="icon-only" />
+      </IonButton>
+    </div>
   );
 };
 
