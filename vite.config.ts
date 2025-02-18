f5d234c9abacba7409834ecf74f6dd007be45617
Daniel Holterhaus <daniel.holterhaus@gmail.com>
2025-02-18 15:27:09 +0100

    project is building, but there's still an error in App.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
